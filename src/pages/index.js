c2488d45bba1f8877327482a7037ab5087f25a0b
Jakov Spahija <jspahi00@fesb.hr>
2020-10-16 23:13:51 +0200

    Creating TitlePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
  
 import HeaderFooterLayout from "../layout/headerFooter"
- 
+import TitlePage from '../modules/TitlePage' 
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     {/* Body components here! */}
