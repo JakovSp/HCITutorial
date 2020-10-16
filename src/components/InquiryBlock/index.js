60dceaf2441097c4e2aa685ee536bd7500335ca1
Jakov Spahija <jspahi00@fesb.hr>
2020-10-16 23:19:18 +0200

    Creating TitleImage

--- src/components/InquiryBlock/index.js
+++ src/components/InquiryBlock/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
  
-import styles from './styles.module.css'
+import styles from './style.module.css'
  
 const TitleImage = () => (
     <section className={styles.titleImage}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
  
 import HeaderFooterLayout from "../layout/headerFooter"
-import TitlePage from '../modules/TitlePage' 
+import TitlePage from '../modules/TitlePage'
+
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
     {/* Body components here! */}
